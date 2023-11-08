e30618a62ee462131f94261d56ef4ed076d35bda
Dirk Dühr <dirk@duehr.de>
2023-11-08 18:03:23 +0100

    GitHub-Link added to the top navigation bar

--- frontend/src/routes/components/TopNavigationBar.tsx
+++ frontend/src/routes/components/TopNavigationBar.tsx
@@ -1,19 +1,29 @@
-import swagger from '../../assets/swagger-icon.png'
+import githubIcon from '../../assets/github-icon.png'
+import swaggerIcon from '../../assets/swagger-icon.png'
 
 export default function TopNavigationBar() {
     return (
         <nav id="top-navigation" className="navbar navbar-expand-lg">
             <div className="container-fluid py-n4">
-                <a className="navbar-brand fw-bold"
-                    href="https://www.youtube.com/channel/UCX9EuLuarfDjJRYt9l0Bbjg"
-                    target="_blank">
-                    <span className="ff-baby-kruffy">DEXTER'S</span> <span className="ff-data-70">Laboratory</span>
-                </a>
-                <a className="navbar-brand"
-                    href={`${import.meta.env.VITE_API_ENDPOINT}/swagger-ui.html`}
-                    target="_blank">
-                    <img src={swagger} />
-                </a>
+                <div>
+                    <a className="navbar-brand fw-bold"
+                        href="https://www.youtube.com/channel/UCX9EuLuarfDjJRYt9l0Bbjg"
+                        target="_blank">
+                        <span className="ff-baby-kruffy pe-1">DEXTER'S</span> <span className="ff-data-70">LABORATORY</span>
+                    </a>
+                </div>
+                <div className="logos">
+                    <a className="navbar-brand"
+                        href="https://github.com/z0lgl428/dexters-lab"
+                        target="_blank">
+                        <img src={githubIcon} />
+                    </a>
+                    <a className="navbar-brand"
+                        href={`${import.meta.env.VITE_API_ENDPOINT}/swagger-ui.html`}
+                        target="_blank">
+                        <img src={swaggerIcon} />
+                    </a>
+                </div>
             </div>
         </nav>
     )
