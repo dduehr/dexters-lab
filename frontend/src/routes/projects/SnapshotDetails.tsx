68f22cdc6b3d75a1da8abb79cd06e11ba4104258
Dirk Dühr <dirk@duehr.de>
2023-11-08 22:16:45 +0100

    BranchDetails and SnapshotDetails completed

--- frontend/src/routes/projects/SnapshotDetails.tsx
+++ frontend/src/routes/projects/SnapshotDetails.tsx
@@ -1,35 +1,39 @@
+import { useLoaderData } from "react-router-dom";
+import { Snapshot } from "../../generated/openapi/projects";
+
 export default function SnapshotDetails() {
+    const snapshot = useLoaderData() as Snapshot
+
     return (
         <>
-            <h3>Snapshot from 02.11.2023 09:47:29</h3>
+            <h3>Snapshot</h3>
             <form>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Identifier</label>
-                    <input type="text" className="form-control" id="name" readOnly
-                        value="b48b49e3-1e70-4ce1-b5f2-abee311268f8" />
+                    <input type="text" className="form-control" id="name" readOnly value={snapshot.id} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="data" className="form-label">Data</label>
-                    <textarea className="form-control" id="data" rows={3} readOnly value="Some data ..." />
+                    <textarea className="form-control" id="data" rows={3} readOnly value={snapshot.data} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="comment" className="form-label">Comment</label>
-                    <textarea className="form-control" id="comment" rows={3} readOnly value="Some comments ..." />
+                    <textarea className="form-control" id="comment" rows={3} readOnly value={snapshot.comment} />
                 </div>
                 <div className="mb-3">
                     <label htmlFor="created-by" className="form-label">Author</label>
-                    <input type="text" className="form-control" id="created-by" readOnly value="usera" />
+                    <input type="text" className="form-control" id="created-by" readOnly value={snapshot.createdBy} />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="created-at" className="form-label">Created</label>
-                    <input type="text" className="form-control" id="created-at" readOnly value="02.11.2023 09:47:29" />
+                    <input type="text" className="form-control" id="created-at" readOnly value={snapshot.createdAt} />
                 </div>
                 <div className="card p-3">
                     <h5>Parent</h5>
                     <div className="mb-3">
                         <label htmlFor="parent" className="form-label">Identifier</label>
                         <input type="text" className="form-control" id="parent" readOnly
-                            value="b48b49e3-1e70-4ce1-b5f2-abee311268f8" />
+                            value="TODO" />
                     </div>
                 </div>
             </form>
